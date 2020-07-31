a16c39d3f359c4b8e1cff880531597070a9ca033
Helena Kolodko <helena.kolodko@gmail.com>
2020-07-31 14:00:05 +0300

    Add task 1.1

--- homework-1/task-1.1/index.js
+++ homework-1/task-1.1/index.js
@@ -1,1 +1,6 @@
-console.log("Hello, World!");
+process.stdin.on('data', (input) => {
+    let reversed = input.reverse().toString();
+    let result = reversed.replace(/^[\r\n]+/, '');
+
+    process.stdout.write(`${result}\n\n`);
+});
