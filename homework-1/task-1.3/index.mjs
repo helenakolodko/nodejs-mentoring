c24ad12499befd9d1780b5492a5ad3e3097b53f7
Helena Kolodko <helena.kolodko@gmail.com>
2020-08-04 11:29:38 +0300

    Fix no result folder. Add processing files in folder

--- homework-1/task-1.3/index.mjs
+++ homework-1/task-1.3/index.mjs
@@ -1,19 +1,47 @@
 import fs from 'fs';
 import csv from 'csvtojson';
+import path from 'path';
 
 const csvFolder = './csv';
 const resultFolder = './result';
 
-const writeStream = fs.createWriteStream(resultFolder + '/test1.txt');
-writeStream.on('error', (error) => {        
-    console.log(`Write error ${error}`);
+chechResultFolder();
+
+fs.readdir(csvFolder, function (err, files) {
+    if (err) {
+        return console.log(`Unable to scan directory: ${err}`);
+    } 
+    files.forEach(function (file) {
+        console.log(`Convert file '${file}'`); 
+        let resultFile = path.join(resultFolder, path.basename(file, path.extname(file)) + '.txt')
+        let csvFile = path.join(csvFolder, file);
+        convertFile(csvFile, resultFile);
+    });
 });
 
-csv()
-    .on('error', (error) => {        
-        console.log(`Conversion error: ${error}`);
-    })
-    .fromFile(csvFolder + '/test1.csv')
-    .subscribe((json, lineNumber) => {
-        writeStream.write(JSON.stringify(json) + '\n');
+function convertFile(csvFile, resultFile) {
+    const writeStream = fs.createWriteStream(resultFile);
+    writeStream.on('error', (error) => {
+        console.log(`Write error ${error}`);
     });
+
+    csv()
+        .on('error', (error) => {
+            console.log(`Conversion error: ${error}`);
+        })
+        .fromFile(csvFile)
+        .subscribe((json, lineNumber) => {
+            writeStream.write(JSON.stringify(json) + '\n');
+        });
+}
+
+function chechResultFolder() {
+    try {
+        if (!fs.existsSync(resultFolder)) {
+            fs.mkdirSync(resultFolder);
+        }
+    }
+    catch (error) {
+        console.error(`Unable to create result folder: ${error}`);
+    }
+}
