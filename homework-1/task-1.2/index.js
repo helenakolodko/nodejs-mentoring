c972e6573c5a833ffdaef2412f2274974b34d76a
Helena Kolodko <helena.kolodko@gmail.com>
2020-08-04 12:18:12 +0300

    Fix read whole file content

--- homework-1/task-1.2/index.js
+++ homework-1/task-1.2/index.js
@@ -1,42 +1,82 @@
 const fs = require('fs');
 const csv = require('csvtojson');
 const path = require('path');
+const { pipeline } = require('stream');
 
 const csvFolder = './csv';
-const resultFolder = './result';
+const resultFolder = './result-whole';
+const resultLineByLineFolder = './result-line-by-line';
 
-chechResultFolder();
+chechResultFolder(resultFolder);
+chechResultFolder(resultLineByLineFolder);
 
 fs.readdir(csvFolder, function (err, files) {
     if (err) {
         return console.log(`Unable to scan directory: ${err}`);
     } 
     files.forEach(function (file) {
-        console.log(`Converting file '${file}'`); 
+        console.log(`Converting file ${file}`); 
         let resultFile = path.join(resultFolder, path.basename(file, path.extname(file)) + '.txt')
         let csvFile = path.join(csvFolder, file);
 
-        convertFile(csvFile, resultFile);
+        convertWholeFile(csvFile, resultFile);
+        
+        resultFile = path.join(resultLineByLineFolder, path.basename(file, path.extname(file)) + '.txt')
+
+        convertFileLineByLine(csvFile, resultFile);
     });
 });
 
-function convertFile(csvFile, resultFile) {
-    const writeStream = fs.createWriteStream(resultFile);
-    writeStream.on('error', (error) => {
-        console.log(`Write error ${error}`);
-    });
+function convertFileLineByLine(csvFile, resultFile) {
+    let readStream = fs.createReadStream(csvFile);
+    let writeStream = fs.createWriteStream(resultFile);
 
-    csv()
+    pipeline(readStream, csv(), writeStream, 
+    (err) => {
+        if (err) {
+          console.error(err);
+        } 
+    });
+}
+
+function convertWholeFile(csvFile, resultFile) {
+    let readStream = fs.createReadStream(csvFile);
+    let fileContent = [];
+
+    readStream.on('data', (chunk) => {
+        fileContent.push(chunk);        
+    })
+    .on('error', (error) => {
+        console.log(`Read error: ${error}`);
+    })
+    .on('end', () => {
+        csv()
         .on('error', (error) => {
             console.log(`Conversion error: ${error}`);
         })
-        .fromFile(csvFile)
-        .subscribe((json, lineNumber) => {
-            writeStream.write(JSON.stringify(json) + '\n');
-        });
+        .fromString(fileContent.toString())
+        .then((json) => {
+            let transformed = transformJson(json);
+
+            let writeStream = fs.createWriteStream(resultFile);
+            writeStream.on('error', (error) => {
+                console.log(`Write error ${error}`);
+            });  
+            writeStream.write(transformed);
+        }); 
+    });       
 }
 
-function chechResultFolder() {
+function transformJson(json){
+    let result = '';
+    json.forEach(item => {
+        result += JSON.stringify(item) + '\n';
+    });
+    return result;
+}
+
+
+function chechResultFolder(resultFolder) {
     try {
         if (!fs.existsSync(resultFolder)) {
             fs.mkdirSync(resultFolder);
