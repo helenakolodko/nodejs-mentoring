7a7318dd3ebdc59ec1325fd867f630bb78b7a244
Helena Kolodko <helena.kolodko@gmail.com>
2020-09-03 13:50:20 +0300

    Add try catch to controller

---
+++ api/src/scripts/users.sql
@@ -0,0 +1,24 @@
+-- Table: public.users
+
+-- DROP TABLE public.users;
+
+CREATE TABLE public.users
+(
+    id uuid NOT NULL,
+    login character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    password character varying(50) COLLATE pg_catalog."default" NOT NULL,
+    age integer NOT NULL,
+    "isDeleted" boolean NOT NULL,
+    CONSTRAINT users_pkey PRIMARY KEY (id)
+)
+
+TABLESPACE pg_default;
+
+ALTER TABLE public.users
+    OWNER to postgres;
+	
+INSERT INTO users (id, login, password, age, "isDeleted") VALUES (uuid_generate_v4(),'BruceS', 'password1', 45, false);
+INSERT INTO users (id, login, password, age, "isDeleted") VALUES (uuid_generate_v4(), 'RobertP', 'password2', 57, false);
+INSERT INTO users (id, login, password, age, "isDeleted") VALUES (uuid_generate_v4(),'SFender ', 'password3', 23, false);             
+INSERT INTO users (id, login, password, age, "isDeleted") VALUES (uuid_generate_v4(), 'DavidG', 'password4', 12, false);
+INSERT INTO users (id, login, password, age, "isDeleted") VALUES (uuid_generate_v4(), 'JBonham', 'password5', 32, false);
